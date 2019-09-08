d1cd3834a1b390af7d0de2bf580c120cec0cf143
roakoth <roseakoth2015@gmail.com>
2019-09-08 15:37:47 +0300

    Initial commit

---
+++ animator/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.sriyank.animationsdemo
+
+import android.animation.AnimatorInflater
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+	}
+
+	fun rotateAnimation(view: View) {
+
+		val rotateAnimator = AnimatorInflater.loadAnimator(this, R.animator.rotate)
+		rotateAnimator?.apply {
+			setTarget(targetImage)
+			start()
+		}
+	}
+
+	fun scaleAnimation(view: View) {
+
+		val scaleAnimator = AnimatorInflater.loadAnimator(this, R.animator.scale)
+		scaleAnimator?.apply {
+			setTarget(targetImage)
+			start()
+		}
+	}
+
+	fun translateAnimation(view: View) {
+
+		val translateAnimator = AnimatorInflater.loadAnimator(this, R.animator.translate)
+		translateAnimator.apply {
+			setTarget(targetImage)
+			start()
+		}
+	}
+
+	fun fadeAnimation(view: View) {
+
+		val fadeAnimator = AnimatorInflater.loadAnimator(this, R.animator.alpha)
+		fadeAnimator.setTarget(targetImage)
+		fadeAnimator.start()
+	}
+}
